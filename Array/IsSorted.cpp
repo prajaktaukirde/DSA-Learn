d09d975b1c22f595588e50867566fb8ed928b5f0
Prajakta u <165979795+prajaktaukirde@users.noreply.github.com>
2026-03-24 20:09:14 +0530

    Added check if array is sorted (O(n) time, O(1) space)

---
+++ Array/IsSorted.cpp
@@ -0,0 +1,13 @@
+class Solution {	
+public:
+    bool isSorted(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
